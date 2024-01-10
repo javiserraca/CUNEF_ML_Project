7501da2feaccdcc2b9599dee6058ac3b53fbc53b
javiserraca <81261849+javiserraca@users.noreply.github.com>
2024-01-10 13:34:09 +0100

    Update funciones_auxiliares.py

--- practica0/notebooks/funciones_auxiliares.py
+++ practica0/notebooks/funciones_auxiliares.py
@@ -111,31 +111,6 @@ def get_deviation_of_mean_perc(df, list_var_continuous, target, multiplier):
         
     return df_final
 
-
-
-#def get_corr_matrix(dataset = None, metodo = 'pearson', size_figure = [10,8]):
-#    # Para obtener la correlación de Spearman, sólo cambiar el metodo por 'spearman'
-#
-#    if dataset is None:
-#        print(u'\nHace falta pasar argumentos a la función')
-#        return 1
-#    sns.set(style="white")
-#    # Compute the correlation matrix
-#    corr = dataset.corr(method=metodo) 
-#    # Set self-correlation to zero to avoid distraction
-#    for i in range(corr.shape[0]):
-#        corr.iloc[i, i] = 0
-#    # Set up the matplotlib figure
-#    f, ax = plt.subplots(figsize=size_figure)
-#    # Draw the heatmap with the mask and correct aspect ratio
-#    sns.heatmap(corr, center=0,
-#                square=True, linewidths=.5,  cmap ='viridis' ) #cbar_kws={"shrink": .5}
-#    plt.show()
-#    
-#    return 0
-
-
-
 def get_corr_matrix(dataset = None, metodo = 'pearson', size_figure = [10,8]):
     # Para obtener la correlación de Spearman, sólo cambiar el metodo por 'spearman'
 
